78898be28a561cf725f4ebf9e4c088317590560b
Chong Guo <armourg@sfu.ca>
2018-01-14 12:52:33 -0800

    fixed autolayout bug

--- iOS/RightHere/RHConsts.swift
+++ iOS/RightHere/RHConsts.swift
@@ -3,7 +3,7 @@
 // Copyright (c) 2018 DDPnwHacks2018. All rights reserved.
 //
 
-let ip: String = "10.19.134.99"
+let ip: String = "52.37.144.49"
 
 let apiGetPost: String = "http://\(ip):3000/posts"
 let apiCreatePost: String = "http://\(ip):3000/posts/create"
